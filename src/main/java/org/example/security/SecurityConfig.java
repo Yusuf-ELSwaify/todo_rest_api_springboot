6ec4e130e651eecf37d9457c1c619a45e344758a
Yusuf-ELSwaify <yusufelswaify12@gmail.com>
2023-07-27 11:15:15 +0300

    inmemory user authorization

---
+++ src/main/java/org/example/security/SecurityConfig.java
@@ -0,0 +1,65 @@
+package org.example.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig{
+/*	@Bean
+	PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		//return new BCryptPasswordEncoder(20);
+	}*/
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails userDetails = User.builder()
+				.username("yusuf")
+				.password("{noop}pass")
+				.roles("USER")
+				.build();
+		UserDetails userDetails2 = User.builder()
+				.username("yusuf2")
+				.password("{noop}pass2")
+				.roles("USER2")
+				.build();
+		return new InMemoryUserDetailsManager(userDetails, userDetails2);
+	}
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+		return http.authorizeHttpRequests(auth -> {
+					auth.requestMatchers( HttpMethod.DELETE, "/api/v1/todos/*")
+							.hasAnyRole("USER2");
+					auth.requestMatchers( HttpMethod.POST, "/api/v1/todos/*")
+							.hasAnyRole("USER");
+					auth.requestMatchers( HttpMethod.GET, "/api/v1/todos/1")
+							.hasAnyRole("USER");
+					auth.requestMatchers( HttpMethod.GET, "/api/v1/todos/*")
+							.permitAll();
+/*					auth.requestMatchers(  "/**")
+							.permitAll();*/
+				})
+				.httpBasic(Customizer.withDefaults())
+				.csrf(AbstractHttpConfigurer::disable)
+				.cors(cors -> cors.disable())
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.build();
+	}
+}
